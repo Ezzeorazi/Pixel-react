b5733dfdfb159e1ff298c2388474ccaf90e4d1f2
Ezzeorazi <ezequiel.orazi90@gmail.com>
2024-01-14 20:15:07 -0300

    idioma ingles

--- src/english/layout/LayoutPublicEn.jsx
+++ src/english/layout/LayoutPublicEn.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavbarEn from '../components/NavbarEn'
 import FooterEn from '../components/FooterEn'
+import { useEffect } from 'react'
  
 
 export const LayoutPublicEn = () => {
+  useEffect(() => {
+    document.documentElement.lang = 'en';
+    document.querySelector('meta[name="description"]').setAttribute("content", "Pixel Maker is a digital design agency in Argentina, specialized in web development, graphic design and digital marketing, transform your brand with us!");
+  }, []);
   return (
     <>
     
--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Footer from '../components/Footer.jsx'
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
  
 
 export const LayoutPublic = () => {
+  useEffect(() => {
+    document.documentElement.lang = 'es';
+    document.querySelector('meta[name="description"]').setAttribute("content", "Pixel Maker es una agencia de diseño digital en Argentina, especializada en desarrollo web, diseño gráfico y marketing digital. ¡Transforma tu marca con nosotros!");
+  }, []);
   return (
     <>
     
